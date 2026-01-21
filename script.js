31a66e7743a3b47d8e5331efa8ab390e1156049d
Motosio <meihaisenben@gmail.com>
2026-01-21 10:12:52 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,39 @@
 console.log("席替え準備完了");
+
+const images = [
+  "faces/スウ.png",
+  "faces/フューム.png",
+  "faces/フェノール.png",
+  "faces/レオナ.png"
+];
+
+const board = document.getElementById("board");
+const button = document.getElementById("shuffleBtn");
+
+function shuffle(array) {
+  for (let i = array.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [array[i], array[j]] = [array[j], array[i]];
+  }
+}
+
+function render() {
+  board.innerHTML = "";
+  images.forEach(src => {
+    const div = document.createElement("div");
+    div.className = "seat";
+
+    const img = document.createElement("img");
+    img.src = src;
+
+    div.appendChild(img);
+    board.appendChild(div);
+  });
+}
+
+button.addEventListener("click", () => {
+  shuffle(images);
+  render();
+});
+
+render();
